eda4f750ac9a967df6b77a0d7730e3166f8f12dd
whodanyalahmed <daniahmedkhatri@gmail.com>
2022-04-25 14:40:30 +0500

    application.py added with client requirements

--- application.py
+++ application.py
@@ -2,18 +2,17 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+import pandas as pd
 load_dotenv(dotenv_path='.env')
 # get bearer_key from .env file
 bearer_token = os.environ["BEARER_KEY"]
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 # bearer_token = "AAAAAAAAAAAAAAAAAAAAAJdDaQEAAAAAh%2FezfGwa9sJo1g9PeVlvIO2g3Ic%3DTAyvL8VKOS7zDe5lXtB3tXyCAFMMiaAdTiBz8eyTebp4ElMPtx"
-# read xlsx file and get REAL NAME column
 
+usernames = pd.read_excel('data.xlsx', sheet_name='Sheet1')['REAL NAME']
+print(usernames)
 
-
-
-search_url = "https://api.twitter.com/2/tweets/search/recent"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
@@ -40,22 +39,24 @@ def connect_to_endpoint(url, params):
 
 
 def main(following, followers, tweet_Count):
-    json_response = connect_to_endpoint(search_url, query_params).get("data")
+
+    for username in usernames:
+        username = username.strip()  # remove whitespace
+        profile_url = "https://api.twitter.com/2/users/by/username/{}?user.fields=public_metrics".format(
+            username.replace("@", ""))
+
+        json_response = requests.get(
+            profile_url, auth=bearer_oauth).json().get("data")
+        id = json_response.get("id")
+
     # get all the author_id from the json response
-    author_ids = [tweet.get("author_id") for tweet in json_response]
-    print(json.dumps(author_ids, indent=4, sort_keys=True))
+    # author_ids = [tweet.get("author_id") for tweet in json_response]
+        print(json.dumps(id, indent=4, sort_keys=True))
 
-    for author_id in author_ids:
-        user_url = f"https://api.twitter.com/2/users/{author_id}?user.fields=public_metrics"
-        user_response = requests.get(user_url, auth=bearer_oauth).json().get(
-            "data").get("public_metrics")
-        followers_count = user_response.get("followers_count")
-        print(json.dumps(user_response, indent=4, sort_keys=True))
-        if(int(followers_count) >= 1000):
-
-            followers.append(followers_count)
-            following.append(user_response.get("following_count"))
-            tweet_Count.append(user_response.get("tweet_count"))
+    # for author_id in author_ids:
+    #     user_url = f"https://api.twitter.com/2/users/{author_id}?user.fields=public_metrics"
+    #     user_response = requests.get(user_url, auth=bearer_oauth).json()
+    #     print(json.dumps(user_response, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
@@ -65,6 +66,3 @@ if __name__ == "__main__":
     following = []
     tweet_Count = []
     main(followers, following, tweet_Count)
-    print("following: ", following)
-    print("followers: ", followers)
-    print("number of tweets:", tweet_Count)
